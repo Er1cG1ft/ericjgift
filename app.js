eaa78c173818f9b048f4f15523a56de42e7281d9
giftsoccer14 <giftsoccer14@gmail.com>
2016-07-06 16:46:11 -0400

    menu and photos

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var main = function() {
-  
   $(".proposal").hide();
   $(".inquiry").show();
   
+  $("#dets").click(function() {
+    $("#dets").animate({opacity: "0"},100);
+    $("#navmenu").animate({left: "0px"});
+  });
+  
+  $("#close").click(function() {
+    $("#navmenu").animate({left: "-300px"});
+    $("#dets").animate({opacity: "1"},400);
+  });
+  
+  $(".photo").click(function(e) {
+    $(".content").addClass("blurred");
+    $("." + e.target.id).removeClass("hidden");
+  });
+  
   $('.contactform').change(function(e) {
     var tempScrollTop = $(window).scrollTop();
-    
-    
+
     if($('#proposal').is(":checked")){
       $(window).scrollTop(tempScrollTop);
       $(".inquiry").fadeOut();
@@ -30,7 +43,6 @@ var main = function() {
       //});
     }
   });
- 
 }
  
 $(document).ready(main);
